ec9cdffabb41e3bd8c6337b8c44f18ccadeeb66b
zana suleiman <zana.suleiman.44@gmail.com>
2022-11-09 23:35:01 +0300

    Added Remove Sidebar

--- js/script.js
+++ js/script.js
@@ -6,10 +6,14 @@ let getPageContent = document.querySelector(".page-content");
 
 getBarItem.onclick = () => {
   getSideBar.style = "transform: translateX(0px);width:220px";
+  getSideBar.classList.add("sidebar-active");
 };
 getXmark.onclick = () => {
   getSideBar.style =
     "transform: translateX(-220px);width:220px;box-shadow:none;";
+  if (getSideBar.classList.contains("sidebar-active")) {
+    getSideBar.classList.remove("sidebar-active");
+  }
 };
 
 window.addEventListener("load", () => {
@@ -18,3 +22,17 @@ window.addEventListener("load", () => {
     getPageContent.style.display = "grid";
   }, 1000);
 });
+document.onclick = (e) => {
+  if (getSideBar.classList.contains("sidebar-active")) {
+    if (
+      !e.target.classList.contains("bar-item") &&
+      !e.target.classList.contains("sidebar") &&
+      !e.target.classList.contains("brand") &&
+      !e.target.classList.contains("brand-name")
+    ) {
+      getSideBar.style =
+        "transform: translateX(-220px);width:220px;box-shadow:none;";
+      getSideBar.classList.remove("sidebar-active");
+    }
+  }
+};
